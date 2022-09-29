e2d988d2a0f6ff52daa59a54b6aca23e308d29f7
Sidorenko Maksim <sir.sidorenkomv@yandex.ru>
2022-09-29 08:15:07 +0300

    init

---
+++ src/main/java/com/sale_accounting/SaleAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.sale_accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleAccountingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sale_accounting/SaleAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sale_accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleAccountingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
