0f24ecd8ebd5fbb0bebd71cc63d41c7449a7a8ae
Sidorenko Maksim <sir.sidorenkomv@yandex.ru>
2022-11-09 07:59:56 +0300

    init

--- src/main/java/com/kata/SaleAccountingApplication.java
+++ src/main/java/com/kata/SaleAccountingApplication.java
@@ -1,4 +1,4 @@
-package com.sale_accounting;
+package com.kata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/kata/SaleAccountingApplicationTests.java
+++ src/test/java/com/kata/SaleAccountingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sale_accounting;
+package com.kata;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
